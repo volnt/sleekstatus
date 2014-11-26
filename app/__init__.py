eddd7478d6618e075ce3e069b1b7cb972b06e057
Florent Espanet <florent.esp@gmail.com>
2014-11-27 00:34:53 +0100

    Update setup.py so it does not load the app.
    There was a bug in the setup.py script that made installation
    impossible.  The setup.py script was importing app/__init__.py which was
    importing flask.  Flask being a package that needs to be installed the
    installation did not work.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
-__version__ = "0.1"
-
 from flask import Flask
 import redis
 
--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
-from app import __version__
 
 setup(
     name='sleekstatus',
-    version=__version__,
+    version="0.1.0",
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     include_package_data=True,
