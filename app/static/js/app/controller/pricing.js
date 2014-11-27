7a92bf6de889d568f9637c3d0e704d8e6a0b6a02
Florent Espanet <florent.esp@gmail.com>
2014-11-27 04:00:17 +0100

    Update pricing page.
    Add login form and rework style.

--- app/static/js/app/controller/pricing.js
+++ app/static/js/app/controller/pricing.js
@@ -2,11 +2,22 @@ app.controller("PricingCtrl", function($scope, $http, $routeParams, $location, A
 
   $scope.Alert = Alert;
   $scope.Auth  = Auth;
+  $scope.hashlib = new Rusha();
+
+  $scope.login = function(email, password) {
+    Auth.set(email, $scope.hashlib.digest(password));
+    Auth.verify(function() {
+      $location.url("/dashboard");
+    }, function() {
+      console.log("Error when logging in.");
+    });
+  }
 
   $scope.main = function() {
     /*
     * Entry point of the controller.
     */
+    $scope.Auth.load();
     $(".navbar").addClass("navbar-default").removeClass("navbar-transparent");
   }();
 });
